0fd9afc28f9fe9689714b359a7c9457b0d7e11ec
codermast <codermast@qq.com>
2022-11-30 22:16:11 +0800

    订单详情

---
+++ src/main/java/com/codermast/takeoutfood/service/OrderDetailService.java
@@ -0,0 +1,7 @@
+package com.codermast.takeoutfood.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.codermast.takeoutfood.entity.OrderDetail;
+
+public interface OrderDetailService extends IService<OrderDetail> {
+}
